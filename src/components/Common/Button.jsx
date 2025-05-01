0585461385053fbef3e69ca31505f4fd9c482d43
Anto <anto32299@gmail.com>
2025-05-01 09:32:30 +0800

    fix button

--- src/components/Common/Button.jsx
+++ src/components/Common/Button.jsx
@@ -13,9 +13,9 @@ export default function Button({text, direction, reverse, delay}) {
         threshold={0.2}
         delay={delay}
         >
-        <button className='flex gap-1 items-center mt-8 px-6 py-3 border border-white text-white font-poppins text-xl rounded-lg hover:bg-primary active:bg-primary-dark hover:scale-105 transition cursor-pointer'>
+        <button className='flex gap-1 items-center mt-8 px-6 py-3 border border-white text-white font-poppins md:text-xl rounded-lg hover:bg-primary active:bg-primary-dark hover:scale-105 transition cursor-pointer'>
             {text}
-            <svg className="ml-2 -mr-1 w-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            <svg className="ml-2 -mr-1 w-5 md:w-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
         </button>
     </AnimatedContent>
  )
